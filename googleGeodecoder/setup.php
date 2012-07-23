07f680017242fa7435b1db6adacd6313966c6732
Rakshith Shettigar <rvshettigar.89@gmail.com>
2012-07-22 18:54:35 -0700

    Update links

--- googleGeodecoder/setup.php
+++ googleGeodecoder/setup.php
@@ -1,10 +1,11 @@
 <?php
-	$con = mysql_connect('tunnel.pagodabox.com:3306','kandace','iKvVqxny') ;
+	//$con = mysql_connect('tunnel.pagodabox.com:3306','kandace','iKvVqxny') ;
+	$con = new mysqli('tunnel.pagodabox.com','kandace','iKvVqxny','3306');
 	if (!$con){
 	  die('Could not connect: ' . mysql_error());
 	}
-	mysql_select_db('friendzDB', $con);
+	$con -> select_db('friendzDB');
 	$query = "create table geocoded_address( loc_id varchar(40), address varchar(40), lat double, lng double, primary key(loc_id) )";
-	mysql_query($query);
-	mysql_close($con);
+	$con->query($query);
+	$con->close();
 ?>
