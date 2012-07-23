d8029c7e1688bf8d92b022bc79533f1c92d325c5
Rakshith Shettigar <rvshettigar.89@gmail.com>
2012-07-22 18:41:33 -0700

    Update links

--- header.php
+++ header.php
@@ -3,11 +3,11 @@ require 'fb-phpsdk/facebook.php';
 // Creating the facebook object using appid and app secret
 $facebook = new Facebook(array(
 	// Facebook Credentials for FriendzOnMap
-  //'appId'  => '444724602218418',
-  //'secret' => 'ace7aff8abf062c50b888e47c770cdb7',
+  'appId'  => '444724602218418',
+  'secret' => 'ace7aff8abf062c50b888e47c770cdb7',
  	// Facebook Credentials for FriendzOnMap-Dev
-  'appId'  => '487490984599920',
-  'secret' => 'cc2064e358eb3c34f3e7382fa0548a6e',
+  //'appId'  => '487490984599920',
+  //'secret' => 'cc2064e358eb3c34f3e7382fa0548a6e',
 ));
 
 $user = $facebook -> getUser();
